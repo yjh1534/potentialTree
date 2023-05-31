727329f9d29cca71bcc16627fc4ad01379433fcc
JaehanYoon <jaehan.yoon@g.skku.edu>
2023-05-31 23:07:29 +0900

    Final

--- component/tree.js
+++ component/tree.js
@@ -1,14 +1,24 @@
+function getPathToRoot(node) {
+  const path = [];
+  while (node) {
+    path.unshift(node);
+    node = node.parent;
+  }
+  return path;
+}
+
 class PathconditionTree {
   margin = {
     top: 10,
     right: 10,
     bottom: 10,
-    left: 30
+    left: 50
   }
 
-  constructor(data, svg, tooltip, resetbtn) {
+  constructor(data, svg, tooltip, resetbtn, pcInfo) {
     this.data = data;
     this.svg = d3.select(svg);
+    this.pcTable = d3.select(pcInfo);
     this.container = this.svg.append("g")
       .attr("id", "tree-space")
       .attr("transform", `translate(${this.margin.left}, ${this.margin.top})`);
@@ -18,15 +28,27 @@ class PathconditionTree {
     this.mainTask = "overview"
     this.i = 0;
     this.duration = 750;
-    this.colorMaprange = 10;
+    this.colorMaprange = 4;
+    this.strokeWidth = 8;
+    this.circleR = 10;
+
+    this.root = d3.hierarchy(this.data, d => {
+      return d.children;
+    });
+    this.root.x0 = this.height / 2;
+    this.root.y0 = 0;
 
     this.resetbtn = d3.select(resetbtn)
       .on('click', (e) => {
-        this.treeX = this.margin.left;
-        this.treeY = this.margin.top;
+        this.root = d3.hierarchy(this.data, d => {
+          return d.children;
+        });
+        this.root.x0 = this.height / 2;
+        this.root.y0 = 0;
         this.container.transition()
           .duration(this.duration)
           .attr("transform", `translate(${this.margin.left}, ${this.margin.top})`);
+        this.update(this.root)
       })
 
     this.dragStarted = this.dragStarted.bind(this);
@@ -41,7 +63,7 @@ class PathconditionTree {
     );
 
     this.zoom = d3.zoom()
-      .scaleExtent([1, 4])
+      .scaleExtent([0.5, 4])
       .on('zoom', (e) => {
         this.container.transition()
           .duration(this.duration / 3)
@@ -51,15 +73,10 @@ class PathconditionTree {
     this.container.call(this.zoom);
 
     this.tooltip = d3.select(tooltip);
-    // Create a hierarchical layout
+
     this.treeLayout = d3.tree().size([this.height, this.width]);
 
-    // Create a root node
-    this.root = d3.hierarchy(this.data, d => {
-      return d.children;
-    });
-    this.root.x0 = this.height / 2;
-    this.root.y0 = 0;
+
 
 
 
@@ -67,13 +84,14 @@ class PathconditionTree {
     this.diagonal = this.diagonal.bind(this);
     this.toggleNode = this.toggleNode.bind(this);
 
+
     this.nodeColorMap = d3.scaleSequential().domain([1, this.colorMaprange]).interpolator(d3.interpolatePuRd);
 
     this.colorMapSvg = this.svg.append("g")
       .attr("class", "color-map")
       .attr("transform", `translate(${this.margin.left}, ${this.margin.top})`);
     this.colorMapSvg.append("rect")
-      .attr("width", 210)
+      .attr("width", 20*this.colorMaprange + 10)
       .attr("height", 30)
       .attr("transform", `translate(0,0)`)
       .attr("fill", "lightsteelblue");
@@ -85,10 +103,8 @@ class PathconditionTree {
         .attr("fill", this.nodeColorMap(i));
     }
 
-    // Collapse the root node by default
     this.collapseNode(this.root);
 
-    // Render the tree
     this.update(this.root);
   }
 
@@ -119,29 +135,46 @@ class PathconditionTree {
   }
 
   collapseNode(d) {
-    if (!d.children) {
+    if (d.children) {
       d._children = d.children;
       d._children.forEach(this.collapseNode);
-      d.children = null;
+      if(d.depth > 11)
+        d.children = null;
     }
   }
 
   diagonal(s, d) {
-    const path = `M ${s.y+11.5} ${s.x}
+    const path = `M ${s.y+(this.circleR + this.strokeWidth/2)} ${s.x}
       C ${(s.y + d.y) / 2} ${s.x},
         ${(s.y + d.y) / 2} ${d.x},
-        ${d.y-11.5} ${d.x}`
+        ${d.y-(this.circleR + this.strokeWidth/2)} ${d.x}`
     return path
   }
 
   toggleNode(event, d) {
-    if (this.mainTask == "constraint") {
-      console.log("AAA1");
+    if (this.mainTask == "pruning"){
+      if(d.parent.children){
+        if(d.parent.children.length == 1)
+          d.parent.children = null;
+        else
+          d.parent.children = d.parent.children.filter(child => {return child != d});
+      }
+      this.duration = 0;
+      this.update(this.root);
+      this.duration = 750;
+    }else if (this.mainTask == "subtree"){
+      this.root = d;
+      this.duration = 0;
+      this.update(this.root);
+      this.duration = 750;
+    }else if (this.mainTask == "constraint") {
       this.targetConstraint = d.data.constraint;
+      this.targetConst = d.data.const_constraint;
+      this.taregtLv = d.data.large_constraint;
+      this.targetBoth = d.data.both_constraint;
       this.update(this.root);
     } else {
       if (d.children) {
-        console.log("AAA0");
         d._children = d.children;
         d.children = null;
       } else {
@@ -153,20 +186,25 @@ class PathconditionTree {
   }
 
   fillColor(d) {
-    if (d.parent) {
+    if (d.parent &&  d.parent.children && d.parent.children.length > 1) {
       return this.nodeColorMap(d.data.potential * this.colorMaprange / d.parent.data.potential);
     } else {
-      return "lightsteelblue";
+      return "steelblue";
     }
   }
 
   strokeColor(d) {
-    console.log(this.mainTask)
     switch (this.mainTask) {      
-      case "overview":
-        return this.fillColor(d)
       case "constraint":
-        return d.data.constraint == this.targetConstraint ? "yellow" : this.fillColor(d);
+        if (d.data.constraint == this.targetConstraint){
+          return "lightgreen"
+        } else if(d.data.const_constraint == this.targetConst){
+          return "yellow";
+        } else if(d.data.large_constraint == this.taregtLv){
+          return "lightblue";
+        } else if(d.data.both_constraint == this.targetBoth){
+          return "orange";
+        }
       case "filtering":
         if (d.children) {
           var minPotential = 100000,
@@ -177,14 +215,16 @@ class PathconditionTree {
             })
           switch(this.criteria){
             case "Biased":
-              console.log(this.threshold, (maxPotential - minPotential) * 100 / d.data.potential)
-              return this.threshold < (maxPotential - minPotential) * 100 / d.data.potential ? "red" : this.fillColor(d);
+              return this.threshold < (maxPotential - minPotential) * 100 / d.data.potential ? "orange" : this.fillColor(d);
             case "Balanced":
-              console.log(this.threshold, maxPotential * 100 / d.data.potential)
-              return this.threshold > maxPotential * 100 / d.data.potential ? "green" : this.fillColor(d);
+              return this.threshold > maxPotential * 100 / d.data.potential ? "orange" : this.fillColor(d);
           }
         }
         return this.fillColor(d);
+      case "overview":
+      case "subtree":
+      case "pruning":
+        return this.fillColor(d)
     }
   }
 
@@ -195,7 +235,6 @@ class PathconditionTree {
   }
 
   filterBy(criteria, threshold) {
-    console.log(criteria)
     this.criteria = criteria;
     this.threshold = threshold;
     this.update(this.root);
@@ -219,7 +258,29 @@ class PathconditionTree {
       }, ],
     });
     this.tooltip.style("display", "block");
+
+    const nodePath = getPathToRoot(d);
+    this.displayNodeInformation(nodePath);
   }
+
+  displayNodeInformation(nodePath) {
+    this.pcTable.selectAll("tr").remove();
+
+    const rows = this.pcTable
+      .selectAll("tr")
+      .data(nodePath)
+      .enter()
+      .append("tr");
+
+    rows.append("td").text((d) => d.data.constraint);
+    rows.append("td").text((d) => {
+      if(d.parent)
+        return `${d.data.potential} / ${d.parent.data.potential}`;
+      else
+        return `${d.data.potential} / root`;
+      });
+  }
+
 
   update(source) {
 
@@ -236,13 +297,14 @@ class PathconditionTree {
 
     const node = this.container.selectAll(".node")
       .style("stroke", d => this.strokeColor(d))
+      .style("fill", d => this.fillColor(d))
       .data(nodes, d => {
         return d.id || (d.id = ++this.i);
       })
 
     const nodeEnter = node.enter().append("circle")
       .attr('class', 'node')
-      .attr("r", 10)
+      .attr("r", this.circleR)
       .style("fill", d => this.fillColor(d))
       .style("stroke", d => this.strokeColor(d))
       .attr("transform", d => `translate(${source.y0},${source.x0})`)
